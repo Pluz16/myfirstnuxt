731e219d4c621dc3cf00d0da8a339dac3350b716
Francesco Punta <puntafrancesco@gmail.com>
2023-09-07 16:56:45 +0200

    Testing Pages

---
+++ nuxt-app/pages/about.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <h1>Chi sono</h1>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ nuxt-app/pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>Portfolio di Francesco Punta</h1>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+    h1{
+        text-decoration: double;
+    }
+</style>
